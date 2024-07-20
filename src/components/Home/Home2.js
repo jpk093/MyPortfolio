327c9d2bcc990c2eed63fd5a2393a2191d6aa8d1
jpk093 <jpk093@gmail.com>
2024-07-20 11:52:02 +0530

    MyPortfoilio

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I am a passionate Java developer with extensive experience in building robust backend systems… 🤷‍♂️
+              <br />
+              <br />I am proficient in:
+              <i>
+                <b className="purple"> Java,Spring,Spring MVC,Spring Boot,JDBC,Hibernate,AWS,github,Node,etc. </b>
+              </i>
+              <br />
+              <br />
+              My interests lie in developing scalable backend services and integrating them with modern frontend technologies when needed.
+              <br />
+              <br />
+              I have a strong foundation in:
+              <i>
+                <b className="purple"> object-oriented programming, design patterns, and software architecture.</b>
+              </i>
+              <br />
+              <br />
+              Whenever possible, I also explore new frameworks and technologies to enhance my skill set.
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/jpk093"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/@jpk093"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                   href="https://www.linkedin.com/in/jay-prakash-kumar-990137173"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/official_prakashjay"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
